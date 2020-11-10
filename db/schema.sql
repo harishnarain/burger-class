f10d0ab6f18e1f6e246cad7cdc8ecef1927a5d95
Harish Narain <harishnarain@gmail.com>
2020-11-10 16:21:22 +1100

    Created all models, controllers and boiler plate views from the cats lesson.

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ USE burgers_db;
 CREATE TABLE burgers
 (
     id int NOT NULL AUTO_INCREMENT,
-    burger_name varchar(50) NO NULL,
+    burger_name varchar(50) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (name, devoured) VALUES ("American Hamburger", false);
-INSERT INTO burgers (name, devoured) VALUES ("American Cheeseburger", false);
-INSERT INTO burgers (name, devoured) VALUES ("Vegan Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("American Hamburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("American Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Vegan Burger", false);
