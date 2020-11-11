89a16d9c48e7997b910d09c20827907e52a10bb7
Harish Narain <harishnarain@gmail.com>
2020-11-11 11:53:55 +1100

    Created views and removed functionality for setting devoured state during add operation.

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,10 +1,10 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function () {
+$(() => {
   $(".change-devoured").on("click", function (event) {
     const id = $(this).data("id");
     const newDevoured = $(this).data("newdevoured");
 
-    var newDevouredState = {
+    const newDevouredState = {
       devoured: newDevoured,
     };
 
@@ -12,27 +12,27 @@ $(function () {
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newDevouredState,
-    }).then(function () {
+    }).then(() => {
       console.log("changed devoured to", newDevoured);
       // Reload the page to get the updated list
       location.reload();
     });
   });
 
-  $(".create-form").on("submit", function (event) {
+  $(".create-form").on("submit", (event) => {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newBurger = {
+    const newBurger = {
       name: $("#ca").val().trim(),
-      devoured: $("[name=devoured]:checked").val().trim(),
+      // devoured: $("[name=devoured]:checked").val().trim(),
     };
 
     // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger,
-    }).then(function () {
+    }).then(() => {
       console.log("created new burger");
       // Reload the page to get the updated list
       location.reload();
@@ -40,12 +40,12 @@ $(function () {
   });
 
   $(".delete-burger").on("click", function (event) {
-    var id = $(this).data("id");
+    const id = $(this).data("id");
 
     // Send the DELETE request.
     $.ajax("/api/burgers/" + id, {
       type: "DELETE",
-    }).then(function () {
+    }).then(() => {
       console.log("deleted burger", id);
       // Reload the page to get the updated list
       location.reload();
