14a8c454ac42ab1f563b7e15260ba10feccf43a7
Harish Narain <harishnarain@gmail.com>
2020-11-11 12:11:24 +1100

    Updated connection.js to ready for deployment to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,27 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
+let connection;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "harish",
-  password: "lanier",
-  database: "burgers_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "harish",
+    password: "lanier",
+    database: "burgers_db",
+  });
+}
+
+// The below was used in the local dev environment
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "harish",
+//   password: "lanier",
+//   database: "burgers_db",
+// });
 
 // Make connection.
 connection.connect((err) => {
